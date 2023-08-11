60269fcc399c3919a7eabc45c08cf95fdf3202d1
nil <nil@kobold.dev>
2023-08-11 14:03:28 -0400

    add pixels as our rendering library

--- src/game.rs
+++ src/game.rs
@@ -1,12 +1,17 @@
 use std::time::Duration;
-use winit::{dpi::LogicalSize, event_loop::EventLoop, window::WindowBuilder};
-use winit_input_helper::WinitInputHelper as Input;
 
 use anyhow::Result;
 use hecs::{Entity, World};
+use log::{debug, error};
+use pixels::{Pixels, SurfaceTexture};
+use winit::{dpi::LogicalSize, event::Event, event_loop::EventLoop, window::WindowBuilder};
+use winit_input_helper::WinitInputHelper as Input;
 
 use crate::component::Spatial;
 use crate::util::TickRate;
+
+const WIDTH: u32 = 320;
+const HEIGHT: u32 = 240;
 
 /// Game state.
 pub struct State {
@@ -18,7 +23,7 @@ pub struct State {
 pub fn main_loop() -> Result<()> {
     let event_loop = EventLoop::new();
     let window = {
-        let size = LogicalSize::new(320.0, 240.0);
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
         let scaled = LogicalSize::new(size.width * 3.0, size.height * 3.0);
         WindowBuilder::new()
             .with_title("spellthief")
@@ -30,24 +35,53 @@ pub fn main_loop() -> Result<()> {
 
     let mut input = Input::new();
 
+    let mut pixels = {
+        let size = window.inner_size();
+        let surface = SurfaceTexture::new(size.width, size.height, &window);
+        Pixels::new(WIDTH, HEIGHT, surface)?
+    };
+
     let mut world = World::new();
+
     let player = world.spawn((Spatial::default(),));
+    debug!("player entity generated ({})", player.id());
 
     let mut state = State { world, player };
 
     let mut tick = TickRate::new(Duration::from_millis(20));
     event_loop.run(move |event, _, control_flow| {
+        if let Event::RedrawRequested(_) = event {
+            render(pixels.frame_mut(), &mut state, tick.lag());
+
+            if let Err(why) = pixels.render() {
+                error!("pixels.render failed: {why}");
+                control_flow.set_exit();
+                return;
+            }
+        }
+
         if input.update(&event) {
             tick.step();
+
+            if let Some(size) = input.window_resized() {
+                if let Err(why) = pixels.resize_surface(size.width, size.height) {
+                    error!("pixels.resize_surface failed: {why}");
+                    control_flow.set_exit();
+                    return;
+                }
+            }
+
+            if input.close_requested() {
+                debug!("window close requested");
+                control_flow.set_exit();
+                return;
+            }
+
             while tick.should_update() {
                 update(&mut state, &mut input);
             }
 
-            render(&mut state, tick.lag());
-
-            if input.close_requested() {
-                control_flow.set_exit();
-            }
+            window.request_redraw();
         }
     });
 }
@@ -56,4 +90,16 @@ pub fn main_loop() -> Result<()> {
 fn update(_state: &mut State, _input: &mut Input) {}
 
 /// Render the current game state to the screen.
-fn render(_state: &mut State, _lag: Duration) {}
+///
+/// Texture format for the render frame is assumed to be RGBA8.
+fn render(frame: &mut [u8], _state: &mut State, _lag: Duration) {
+    for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+        let x = (i % WIDTH as usize) as i32;
+        let y = (i / WIDTH as usize) as i32;
+
+        let r = (x * 255 / WIDTH as i32) as u8;
+        let g = (y * 255 / HEIGHT as i32) as u8;
+        let rgba = [r, g, 0xff, 0xff];
+        pixel.copy_from_slice(&rgba);
+    }
+}
