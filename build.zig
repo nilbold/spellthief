ae669a20825ce23410e231330c84b3b73372201c
nil <nil@kobold.dev>
2025-03-22 00:55:13 -0400

    add sdl3 dependency

--- build.zig
+++ build.zig
@@ -6,8 +6,6 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const run_step = b.step("run", "Run executable");
-
     const exe = b.addExecutable(.{
         .name = "spellthief",
         .version = version,
@@ -18,6 +16,14 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
+    const sdl_dep = b.dependency("sdl", .{
+        .target = target,
+        .optimize = optimize,
+        .preferred_link_mode = .static,
+    });
+    const sdl_lib = sdl_dep.artifact("SDL3");
+    exe.root_module.linkLibrary(sdl_lib);
+
     const options = b.addOptions();
     exe.root_module.addOptions("options", options);
 
@@ -26,5 +32,8 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(exe);
 
     const run_exe = b.addRunArtifact(exe);
-    run_step.dependOn(&run_exe.step);
+    run_exe.step.dependOn(b.getInstallStep());
+
+    const run = b.step("run", "Run executable");
+    run.dependOn(&run_exe.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,75 @@
 const std = @import("std");
 const options = @import("options");
 
-pub fn main() void {
-    std.debug.print("placeholder ~ spellthief {}\n", .{options.version});
+const c = @cImport({
+    @cDefine("SDL_DISABLE_OLD_NAMES", {});
+    @cInclude("SDL3/SDL.h");
+    @cInclude("SDL3/SDL_revision.h");
+    @cDefine("SDL_MAIN_HANDLED", {});
+    @cInclude("SDL3/SDL_main.h");
+});
+
+pub fn main() !void {
+    errdefer |err| if (err == error.SdlError) std.log.err("SDL error: {s}", .{c.SDL_GetError()});
+
+    std.debug.print("spellthief version {}\n", .{options.version});
+    std.debug.print("sdl build version {d}.{d}.{d}\n", .{
+        c.SDL_MAJOR_VERSION,
+        c.SDL_MINOR_VERSION,
+        c.SDL_MICRO_VERSION,
+    });
+
+    c.SDL_SetMainReady();
+    if (!c.SDL_SetAppMetadata("spellthief", "0.0.0", "spellthief")) {
+        return error.SdlError;
+    }
+
+    if (!c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO | c.SDL_INIT_GAMEPAD)) {
+        return error.SdlError;
+    }
+    defer c.SDL_Quit();
+
+    const window_w = 320 * 4;
+    const window_h = 240 * 4;
+    if (!c.SDL_SetHint(c.SDL_HINT_RENDER_VSYNC, "1")) {
+        return error.SdlError;
+    }
+
+    const window: *c.SDL_Window, const renderer: *c.SDL_Renderer = wr: {
+        var window: ?*c.SDL_Window = null;
+        var renderer: ?*c.SDL_Renderer = null;
+        if (!c.SDL_CreateWindowAndRenderer("spellthief", window_w, window_h, 0, &window, &renderer)) {
+            return error.SdlError;
+        }
+
+        break :wr .{ window.?, renderer.? };
+    };
+    defer c.SDL_DestroyRenderer(renderer);
+    defer c.SDL_DestroyWindow(window);
+
+    main_loop: while (true) {
+        var event: c.SDL_Event = undefined;
+        while (c.SDL_PollEvent(&event)) {
+            switch (event.type) {
+                c.SDL_EVENT_QUIT => {
+                    break :main_loop;
+                },
+                else => {},
+            }
+        }
+
+        std.time.sleep(10 * std.time.ns_per_ms);
+
+        if (!c.SDL_SetRenderDrawColor(renderer, 0x11, 0x22, 0x33, 0xff)) {
+            return error.SdlError;
+        }
+
+        if (!c.SDL_RenderClear(renderer)) {
+            return error.SdlError;
+        }
+
+        if (!c.SDL_RenderPresent(renderer)) {
+            return error.SdlError;
+        }
+    }
 }
